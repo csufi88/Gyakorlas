4442e5b41891b1d393c551384c678443cf046039
Zsofi <nzsofi062@gmail.com>
2025-03-27 14:18:22 +0100

    0-3 szamok

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function szoveg(szam){
+    return szam === 0 ? 0 : szam;
+    const szamok = {
+        0: "nulla",
+        1: "egy",
+        2: "ketto",
+        3: "harom"
+    };
+    
+
+}
