82eca2f79f25a7683d4dc79cfc3e0bc18957fa60
maurik1830 <nagyemoric@gmail.com>
2025-03-27 14:18:16 +0100

    test.js test script

---
+++ test.js
@@ -0,0 +1,32 @@
+
+import szoveg from "./index.js"
+
+szam = 1
+
+
+switch (szoveg(szam)) {
+    case "egy":
+        console.log("ok");
+        
+        break;
+    case "ketto":
+        console.log("ok");
+        
+        break;
+    case "harom":
+        console.log("ok");
+        
+        break;
+    case "nulla":
+        console.log("ok");
+        
+        break;
+            
+    default:
+        console.log("Sikertelen.");
+        
+        break;
+}
+
+
+
